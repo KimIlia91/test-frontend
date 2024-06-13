e304bb1c56a4a5056a56aeb3dd690e89c1ef343d
paterfamelias@gmail.com <paterfamelias@gmail.com>
2024-06-14 03:04:28 +0600

    export index ts for svg

---
+++ src/assets/insex.ts
@@ -0,0 +1,19 @@
+import backRow from './svg/back-row.svg'
+import email from './svg/email.svg'
+import eyeShowOff from './svg/eye-show-off.svg'
+import heardLike from './svg/heard-like.svg'
+import heardUnlike from './svg/heard-unlike.svg'
+import logout from './svg/logout.svg'
+import phone from './svg/phone.svg'
+import showMoreRowDown from './svg/show-more-row-down.svg'
+
+export {
+    backRow,
+    email,
+    eyeShowOff,
+    heardLike,
+    heardUnlike,
+    logout,
+    phone,
+    showMoreRowDown,
+}
