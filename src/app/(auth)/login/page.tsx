8f3de1bacfabf1415215131bbddaf6f357334a22
paterfamelias@gmail.com <paterfamelias@gmail.com>
2024-06-29 16:07:22 +0600

    added session provider

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,21 @@
+import { auth } from "@/auth"
 import { Metadata } from "next"
 import LoginForm from "@/components/ui/auth/login-form"
+import SessionProviderWrapper from "@/components/hoc/session-provider"
 
 export const metadata: Metadata = {
   title: 'Вход',
 }
 
-const LoginPage = () => {
+const LoginPage = async() => {
+  const session = await auth()
+
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="container flex justify-center">
-        <LoginForm />
+        <SessionProviderWrapper session={session}>
+          <LoginForm />
+        </SessionProviderWrapper>
       </div>
     </main>
   )
