9847e15373b52c3552080cac488ce16ec72fbcd3
paterfamelias@gmail.com <paterfamelias@gmail.com>
2024-06-29 18:21:34 +0600

    added ui common message error for registration

--- src/components/ui/auth/register-form.tsx
+++ src/components/ui/auth/register-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from "next/link"
-import { useFormState, useFormStatus } from "react-dom"
+import { useFormState } from "react-dom"
 import InputForm from "./input-form"
 import PasswordInput from "./password-input"
 import { AuthState, register } from "@/services/auth-service"
@@ -10,11 +10,17 @@ import SubmitButton from "./submit-button"
 export default function RegisterForm() {
     const initialState = { errors: {}, message: null }
     const [state, dispatch] = useFormState<AuthState, FormData>(register, initialState)
-    const { pending } = useFormStatus()
 
     return (
         <div className="max-w-[500px] w-full rounded-[16px] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.08)] bg-white p-4">
             <h1 className="text-xl mb-4 text-center font-bold">Регистрация</h1>
+            <div aria-live="polite" aria-atomic="true">
+                {state?.message &&
+                    <p className="mt-2 text-center text-sm text-red-500" key={state?.message}>
+                        {state?.message}
+                    </p>
+                }
+            </div>
             <form action={dispatch} className="flex flex-col gap-8">
                 <InputForm 
                     id="name"
